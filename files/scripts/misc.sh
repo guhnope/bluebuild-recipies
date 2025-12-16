addd1a622cbabf1931464babb4e457a2bf07aad0
MGS <dane.butler@gmail.com>
2025-12-16 22:03:47 +1000

    try harder to remove bad files

--- files/scripts/misc.sh
+++ files/scripts/misc.sh
@@ -3,6 +3,3 @@ set -euo pipefail
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-rm -rf /usr/share/doc/just
-rm -rf /usr/share/doc/niri
