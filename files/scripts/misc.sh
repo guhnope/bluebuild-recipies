a24a0b258f1dce5ee71a4027f9ff82ea96cd3fb2
MGS <dane.butler@gmail.com>
2025-12-16 21:32:16 +1000

    update misc tasks

--- files/scripts/misc.sh
+++ files/scripts/misc.sh
@@ -4,10 +4,5 @@ QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kerne
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 
-# Remove just doc file
-<<<<<<< HEAD
 rm -rf /usr/share/doc/just
 rm -rf /usr/share/doc/niri
-=======
-rm -rf /usr/share/doc /usr/share/man /usr/share/info
->>>>>>> 7ff77aa40d90b7649b03b7ec82009566b62ba4bb
