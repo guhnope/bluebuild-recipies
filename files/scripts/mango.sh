596faeaafade61c3bfb0863aef32036e45830955
MGS <dane.butler@gmail.com>
2025-12-17 18:38:37 +1000

    executable mango.sh

--- files/scripts/mango.sh
+++ files/scripts/mango.sh
@@ -1,1 +1,5 @@
 chmod 755 /etc/mango/autostart.sh
+echo "Ensure autostart.sh is executable"
+for u in $(awk -F: '$3 >= 1000 {print $1}' /etc/passwd); do
+    chmod 755 /var/home/$u/.config/mango/autostart.sh
+done
