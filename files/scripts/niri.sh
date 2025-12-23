422bffcef7df9d981be093a3a00c191012b5ab51
MGS <dane.butler@proton.me>
2025-12-23 16:53:20 +1000

    split out compositor packages

--- files/scripts/niri.sh
+++ files/scripts/niri.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -euo pipefail
 
-rm -rf /usr/games
-rm -rf /usr/share/doc
--- files/scripts/noctalia.sh
+++ files/scripts/noctalia.sh
@@ -1,4 +1,1 @@
 set -euo pipefail
-mkdir -p /etc/xdg/quickshell
-git clone https://github.com/noctalia-dev/noctalia-shell.git /etc/xdg/quickshell/noctalia
-rm -rf /etc/xdg/quickshell/noctalia/.git/
