31e2c9cd1200a3d709331ff5a685584e6b381bc9
MGS <dane.butler@proton.me>
2026-01-05 15:46:37 +1000

    try adding discord recommendations

--- files/scripts/kernel-post.sh
+++ files/scripts/kernel-post.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+pushd /usr/lib/kernel/install.d
+mv -f 05-rpmostree.install.bak 05-rpmostree.install
+mv -f 50-dracut.install.bak 50-dracut.install
+popd
+
 # Find the latest CachyOS kernel entry
 CACHY_ENTRY=$(grubby --info=ALL | grep -B1 "kernel.*cachyos" | grep ^index | head -1 | cut -d= -f2)
 
