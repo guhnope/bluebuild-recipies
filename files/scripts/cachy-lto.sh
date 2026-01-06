07ce89fec3ac55b980f1ba503edc433f754a4ece
MGS <dane.butler@proton.me>
2026-01-06 20:09:36 +1000

    clean up kernel

--- files/scripts/cachy-lto.sh
+++ files/scripts/cachy-lto.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 dnf -y remove \
-    kernel \
+    kernel kernel-core \
+    kernel-modules kernel-modules-core \
+    kernel-modules-extra kernel-tools-libs \
+    kernel-headers kernel-tools \
     kernel-* &&
+
     rm -r -f /usr/lib/modules/*
 
 dnf -y install --setopt=install_weak_deps=False \
