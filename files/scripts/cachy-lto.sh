7700140ab3113cf41851634dc454d09e07b2d24c
MGS <dane.butler@proton.me>
2026-01-10 11:32:38 +1000

    one line

--- files/scripts/cachy-lto.sh
+++ files/scripts/cachy-lto.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-dnf -y remove \
+#dnf -y remove \
 #    kernel kernel-core kernel-headers kernel-tools 
 #    kernel-modules kernel-modules-core \
 #    kernel-modules-extra kernel-tools-libs \
-    kernel-* &&
-    rm -r -f /usr/lib/modules/*
+dnf -y remove kernel-* &&  rm -r -f /usr/lib/modules/*
 
 dnf -y install --setopt=install_weak_deps=False \
     kernel-cachyos-lto
